d615409a7380e63f2da588c2b843427b82596a3d
Luca De Santis <lucaroma2705@gmail.com>
2023-03-04 23:53:12 +0100

    Add illustration for not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,16 +1,24 @@
+import { TitleLayout } from "@src/layouts";
 import { Link } from "react-router-dom";
+import not_found from "@src/assets/not-found.svg";
 
 function NotFound() {
   return (
-    <div className="flex flex-col items-center gap-4 w-full h-[90%] pt-10">
-      <h1 className="text-2xl">Page Not Found</h1>
-      <Link
-        to="/"
-        className="flex justify-center items-center gap-2 w-32 p-2 rounded-lg font-semibold bg-tiber-500 text-white cursor-pointer hover:bg-tiber-600"
-      >
-        Back Home
-      </Link>
-    </div>
+    <TitleLayout title="Page not found">
+      <div className="flex flex-col justify-center items-center gap-4">
+        <img
+          className="w-48 h-48"
+          src={not_found}
+          alt="not found illustration"
+        />
+        <Link
+          to="/"
+          className="flex justify-center items-center gap-2 w-32 p-2 rounded-lg font-semibold bg-tiber-500 text-white cursor-pointer hover:bg-tiber-600"
+        >
+          Back Home
+        </Link>
+      </div>
+    </TitleLayout>
   );
 }
 
